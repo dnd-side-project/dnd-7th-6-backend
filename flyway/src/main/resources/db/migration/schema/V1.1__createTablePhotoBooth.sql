0eea3d1fd4a8c69238f97b0573c6a20124229932
south-daria <jy.nam@weversecompany.com>
2022-07-21 22:54:51 +0900

    #develop unique key 제거

--- flyway/src/main/resources/db/migration/schema/V1.1__createTablePhotoBooth.sql
+++ flyway/src/main/resources/db/migration/schema/V1.1__createTablePhotoBooth.sql
@@ -8,7 +8,7 @@ CREATE TABLE `photo_booth` (
   `name` varchar(50) NOT NULL COMMENT 'photo booth name',
   `jibun_address` varchar(150) NOT NULL COMMENT 'photo booth jibun address',
   `road_address` varchar(150) NOT NULL COMMENT 'photo booth road address',
-  `point` POINT NOT NULL UNIQUE COMMENT 'geometry',
+  `point` POINT NOT NULL COMMENT 'geometry',
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '등록일시',
   `updated_at` datetime NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정일시',
   PRIMARY KEY (`id`)
