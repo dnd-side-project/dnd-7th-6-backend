724294e7ab8d056f9198639ef5cea627067b9ff2
xx10222 <yjw4960@gmail.com>
2022-10-13 16:47:57 +0900

    :sparkles: [feature/188] 신고 관련 table sql 추가

---
+++ flyway/src/main/resources/db/migration/schema/V1.22__createTableUserReportPost.sql
@@ -0,0 +1,17 @@
+--
+-- Table structure for table `user_report_post`
+--
+DROP TABLE IF EXISTS `user_report_post`;
+ SET character_set_client = utf8mb4;
+CREATE TABLE `user_report_post` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'post_like',
+  `post_id` bigint NOT NULL COMMENT 'post_id',
+  `user_id` bigint NOT NULL COMMENT 'user_id',
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '등록일시',
+  `updated_at` datetime NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정일시',
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (post_id) REFERENCES post(id) ON DELETE CASCADE,
+  FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='포스트 신고 유저 테이블';
+create unique index user_report_post_post_id_user_id_uindex
+    on user_report_post (post_id, user_id);
